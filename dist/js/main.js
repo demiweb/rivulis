df4e13f4f27b97a73ec56f39ac3b3a9096c112e2
modifikult <modifikult2016@gamil.com>
2021-12-07 11:12:11 +0200

    fixed desktop layout

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,13 +3,27 @@ const technologySlider = new Swiper('.technology-slider', {
     loop: false,
     slidesPerView: 3,
     centeredSlides: true,
+
+    on: {
+        init: openAccordion,
+    }
 });
 
-technologySlider.on('slideChange', function (elem) {
-    const accordions = document.querySelectorAll('.slider__accordions .technology')
-    accordions.forEach(item => item.classList.remove('active'))
-    accordions[elem.activeIndex].classList.add('active')
-});
+technologySlider.on('slideChange', openAccordion);
+
+technologySlider.on('mount', openAccordion);
+
+function openAccordion() {
+    console.log(this)
+    let content = [...document.querySelectorAll('.slider__accordions .technology')][this.activeIndex]
+
+    if (content.style.maxHeight) {
+        document.querySelectorAll('.slider__accordions .technology').forEach(accordion => accordion.style.maxHeight = null)
+    } else {
+        document.querySelectorAll('.slider__accordions .technology').forEach(accordion => accordion.style.maxHeight = null)
+        content.style.maxHeight = content.scrollHeight + 'px'
+    }
+}
 
 
 const selectBrochure = document.querySelectorAll('.brochure__select')
